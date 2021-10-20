1d2163671ca663b148e834a0fd57c4591ca41833
Jordan Mata <jordanmata15@yahoo.com>
2021-10-20 15:15:06 -0700

    Corrected the UpdatePrice command object.

--- Bookstore/src/main/UpdatePriceCommand.java
+++ Bookstore/src/main/UpdatePriceCommand.java
@@ -1,9 +1,5 @@
 package main;
 
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.ObjectOutputStream;
-
 public class UpdatePriceCommand extends InventoryCommand{
 	
 	/**
@@ -19,20 +15,6 @@ public class UpdatePriceCommand extends InventoryCommand{
 	
 	@Override
 	public double execute(Inventory toExecuteOn) {
-		File someFile = null;
-		this.appendCommand(someFile);
 		return toExecuteOn.updatePrice(toExecuteWith);
-	}
-	
-	private void appendCommand(File filename) {
-		try {
-			FileOutputStream fileOut = new FileOutputStream(filename, true);
-			ObjectOutputStream objOutput = new ObjectOutputStream(fileOut);
-			objOutput.writeObject(this);
-			objOutput.close();
-			fileOut.close();
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
 	}
 }
